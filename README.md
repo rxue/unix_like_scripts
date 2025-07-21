97bfa39c5cd0d96ae55e958eb7165f1c8c92f5fc
Rui Xue <ruixue.fi@gmail.com>
2025-07-21 23:30:33 +0300

    #24 vim installation script added

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # How to run
-1. clone this project somewhere
-2. `cd` to this project directory, `linux-scripting`
-3. run command `source xxx` 
-
-
+## Prerequisite: install `git` as the first step and then clone this project somewhere
+## 1. `cd` to this project directory, `linux-scripting`
+## 2. `chmod +x ubuntu/install.sh`
+## 3. `sudo ./ubuntu/install.sh`
 # Bash
 [Bash Beginners Guide](http://www.tldp.org/LDP/Bash-Beginners-Guide/Bash-Beginners-Guide.pdf)
 
--- configuration/configure_functions.sh
+++ configuration/configure_functions.sh
@@ -1,16 +1,31 @@
 # Install and configure vim
 # Reference: http://vim.wikia.com/wiki/Indenting_source_code
 configure_vim () {
+  cp /etc/vim/vimrc.local ~/.vimrc
   config_statements=`cat <<EOF
-# Reference: http://tldp.org/LDP/abs/html/here-docs.html
-# Configure VIM on the system level
-set number
-syntax on
-set hlsearch
-set expandtab
-set shiftwidth=2
-set softtabstop=2
-set list # display tab
+# Imitate NerdTree: https://shapeshed.com/vim-netrw/
+let g:netrw_banner = 0
+let g:netrw_liststyle = 3
+let g:netrw_browse_split = 4
+let g:netrw_altv = 1
+let g:netrw_winsize = 25
+augroup ProjectDrawer
+  autocmd!
+  autocmd VimEnter * :Vexplore
+augroup END
 EOF`
-  echo "${config_statements}"
+  echo "${config_statements}" >> ~/.vimrc
+}
+# $1 - name
+# $2 - email
+configure_git () {
+  local _name=$1
+  local _email=$2
+  git config --global user.name "${_name}"
+  git config --global user.email "${_email}"
+  # display colours for example on git status or git diff commands
+  git config --global --add color.ui true
+  # set vim as the default editor of git
+  git config --global core.editor "vim"
+  ssh-keygen -t ed25519 -C "${_email}"
 }
