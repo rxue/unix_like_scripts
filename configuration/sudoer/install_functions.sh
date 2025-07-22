1fac4b364938ea478663a6872352d1422a42a7d0
Rui Xue <ruixue.fi@gmail.com>
2025-07-22 18:35:38 +0300

    #31 docker automatic installation script added

--- configuration/sudoer/install_functions.sh
+++ configuration/sudoer/install_functions.sh
@@ -24,31 +24,6 @@ install_system_monitors () {
   apt-get install strace
   apt-get install htop
 }
-# $1 - user to add as sudoer 
-#add_sudoer () {
-#  user_name=$1
-#  config_statement=$(grep '^root' /etc/sudoers)
-#  [[ ! $(grep '^'${user_name} /etc/sudoers) ]] && echo ${config_statement/root/$user_name} >> /etc/sudoers
-#  
-#}
-
-# Reference: https://docs.docker.com/engine/install/debian/
-install_docker () {
-  # Set up the repository
-  apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-  # Add Docker's Official GPG key
-  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
-  echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io
-}
 # Reference: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 install_aws_cli () {
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
--- ubuntu/install_functions.sh
+++ ubuntu/install_functions.sh
@@ -75,3 +75,23 @@ install_latest_intellij_idea () {
   ln -fs ${idea_bin_dir}/idea /usr/bin/idea
   _add_to_gnome_main_menu idea ${idea_bin_dir}/idea ${idea_bin_dir}/idea.png
 }
+# Reference: https://docs.docker.com/engine/install/debian/
+install_docker () {
+  # Set up the repository
+  apt-get --assume-yes install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  # Add Docker's Official GPG key
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # Install
+  apt-get update
+  apt-get --assume-yes install docker-ce docker-ce-cli containerd.io
+}
