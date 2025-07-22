83b675f9f844c4ec6b6c2ca99436be1f4150055e
Rui Xue <ruixue.fi@gmail.com>
2025-07-22 16:22:17 +0300

    #30 install_maven added to sudo_install.sh

--- ubuntu/sudo_install.sh
+++ ubuntu/sudo_install.sh
@@ -6,4 +6,5 @@ apt-get --assume-yes install vim
 cat configuration/sudoer/templates/vimrc.local |tee /etc/vim/vimrc.local
 install_chrome
 apt-get --assume-yes install openjdk-21-jdk
+install_maven
 install_latest_intellij_idea
