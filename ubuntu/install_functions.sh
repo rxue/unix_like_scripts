079b1a9e87f0a04fa3e8ab35c0807a43d9a637e4
Rui Xue <ruixue.fi@gmail.com>
2025-07-24 17:46:06 +0300

    #31 permission configuration on docker

--- ubuntu/install_functions.sh
+++ ubuntu/install_functions.sh
@@ -51,10 +51,13 @@ install_docker () {
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   chmod a+r /etc/apt/keyrings/docker.gpg
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   # Install
   apt-get update
   apt-get --assume-yes install docker-ce docker-ce-cli containerd.io
+  echo "add docker user group and add the current user to it"
+  newgrp docker
+  usermod -aG docker $USER
 }
+
